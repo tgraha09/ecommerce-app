22b54f240ad27314686a2724970fe12fc61cf4a3
tgraha09 <tfire09@gmail.com>
2023-06-11 13:43:33 -0400

    Refactored utilities code
    Refactored utilities code surrounding network connection and content loading.

---
+++ BlazorApp/CustomAuthStateProvider.cs
@@ -0,0 +1,12 @@
+﻿//https://www.youtube.com/watch?v=Yh16E2u2pio&ab_channel=PatrickGod
+//https://www.c-sharpcorner.com/article/jwt-token-creation-authentication-and-authorization-in-asp-net-core-6-0-with-po/
+namespace BlazorApp
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,3 +1,4 @@
+global using Microsoft.AspNetCore.Components.Authorization;
 using BlazorApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//Authentication Added
+/*builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();*/
 
 await builder.Build().RunAsync();
